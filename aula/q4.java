2547db7cdba251f3ce00309d7d9cbb8214fc76fe
Lex <153548138+alexrhikelme@users.noreply.github.com>
2025-04-28 12:29:31 +0000

    Atividade

---
+++ aula/q4.java
@@ -0,0 +1,12 @@
+package aula;
+
+import java.util.*;
+
+public class q4{
+    public static void main(String[] args){
+        HashSet<String> nomes = new HashSet<>();
+        nomes.add("Ana");
+        nomes.add("Bruno");
+        nomes.add("Carla")
+    }
+}
